dc4504f66ecb0865f27a4e53d7d329e5a922e478
胡正全 <huzhengquan@baidu.com>
2014-06-19 14:55:41 +0800

    1.6.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/huzhengquan/barber"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.jsoup/jsoup "1.7.3"]]
   :main barber.core/foo
   ;:user {:plugins [[lein-clojars "0.9.1"]]}
